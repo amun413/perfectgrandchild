4bf4d89ff941bef2f0ec65c45967d27ee6217ab1
Miffy Chen <mchen15@bu.edu>
2019-03-23 18:02:52 -0400

    Added index.html

--- server.js
+++ server.js
@@ -3,13 +3,14 @@
 const express = require('express');
 
 // Constants
-const PORT = 3000;
+const PORT = 8000;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node.js Sample!\n');
+  // res.send('Hello Node.js Sample!\n');
+  res.send('index.html');
 });
 
 var port = process.env.PORT||PORT;
