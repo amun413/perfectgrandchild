ad5457bc481aa8728040ac3e14d984b381370368
Miffy Chen <mchen15@bu.edu>
2019-03-23 19:46:39 -0400

    Final version of node express.js that works lol

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
-var app = express();
 var path = require('path');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+var app = express();
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(PORT);
+console.log(`Running on http://${HOST}:${PORT}`);
